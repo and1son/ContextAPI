36acf241df801395b4e34abe947f5556db500f39
Andi <andi.basic10@gmail.com>
2022-09-28 22:57:13 +0200

    project setup

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+import Switch from "@material-ui/core/Switch";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles/NavBarStyles";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>Navbar</div>
+        )
+    }
+}
+
+export default Navbar;
