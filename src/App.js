a9bfd44044726e9296a7e33e78e0f1c102fb6216
andison <andi.basic10@gmail.com>
2022-09-29 18:20:18 +0200

    form & form styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
+import Form from "./Form";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Form />
+    </>
   );
 }
 
