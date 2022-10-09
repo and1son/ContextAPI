4236d4a1d818c4e931b1cff512fa5c2212b0f9a1
Andi <andi.basic10@gmail.com>
2022-10-09 21:07:47 +0200

    themecontext hook

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,27 +1,17 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, useState } from "react";
 
 export const LanguageContext = createContext()
 
-export class LanguageProvider extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { language: 'french' }
-        this.changeLanguage = this.changeLanguage.bind(this);
-    }
+export function LanguageProvider(props) {
 
-    changeLanguage(e) {
-        this.setState({
-            language: e.target.value
-        })
-    }
+    const [language, setLanguage] = useState('spanish')
+    const changeLanguage = (e) => setLanguage(e.target.value)
 
-    render() {
-        return (
-            <LanguageContext.Provider value={{ ...this.state, changeLanguage: this.changeLanguage }}>
-                {this.props.children}
-            </LanguageContext.Provider>
-        )
-    }
+    return (
+        <LanguageContext.Provider value={{ language, changeLanguage }}>
+            {props.children}
+        </LanguageContext.Provider>
+    )
 }
 
 export const withLanguageContext = Component => props => (
